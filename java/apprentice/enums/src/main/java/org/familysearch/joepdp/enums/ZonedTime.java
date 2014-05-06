92b15b2d6c3e716a5ac2166d66b5d8398d3e635c
Joe Shull <shullja@familysearch.org>
2014-05-06 16:09:30 -0600

    moved files from old repository

--- java/apprentice/enums/src/main/java/org/familysearch/joepdp/enums/ZonedTime.java
+++ java/apprentice/enums/src/main/java/org/familysearch/joepdp/enums/ZonedTime.java
@@ -37,9 +37,4 @@ public class ZonedTime {
         return "" + getHour() + ":" + getMinute() + ":" + getSecond();
     }
 
-    public static void main(String[] args){
-        ZonedTime pacificEleven42 = new ZonedTime(23, 42, 0, USTimeZones.Pacific);
-        ZonedTime easternTwo42 = pacificEleven42.convertTimeZone(USTimeZones.Eastern, false);
-        System.out.println("When it is " + pacificEleven42 + " in the pacific time zone, it is " + easternTwo42 + " in the eastern time zone.");
-    }
 }
