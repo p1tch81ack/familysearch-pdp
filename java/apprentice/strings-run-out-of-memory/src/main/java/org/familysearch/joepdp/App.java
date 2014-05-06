1ed8fc2c308e370c3a466ecb402144e5dc67d613
Joe Shull <shullja@familysearch.org>
2014-05-06 17:04:34 -0600

    implementation copied over

--- java/apprentice/strings-run-out-of-memory/src/main/java/org/familysearch/joepdp/App.java
+++ java/apprentice/strings-run-out-of-memory/src/main/java/org/familysearch/joepdp/App.java
@@ -1,13 +1,43 @@
 package org.familysearch.joepdp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class App {
+    private static String padTo(String original, int length, char padCharacter){
+        String out = original;
+        for(int i=0; i<length-original.length(); i++) {
+            out = "" + padCharacter + out;
+        }
+        return out;
+    }
+    private static String generateEntry(int index){
+        String numString = "" + index;
+        return padTo(numString, 100, '0');
+    }
+
+    public static void main(String[] args){
+        List<String> listOfStrings = new LinkedList<String>();
+        int i=0;
+        try {
+            while(true){
+                listOfStrings.add(generateEntry(i));
+                i++;
+            }
+        } catch (OutOfMemoryError e) {
+            listOfStrings = null; // give the garbage collector a chance to run if necessary
+            System.err.println("Exception: " + e.getMessage());
+            e.printStackTrace(System.err);
+        } catch (Exception e) {
+            listOfStrings = null; // give the garbage collector a chance to run if necessary
+            System.err.println("Exception: " + e.getMessage());
+            e.printStackTrace(System.err);
+        }
+        if(listOfStrings!=null){
+            System.out.println("Quitting in a completely unanticiped way...");
+        } else {
+            System.out.println("Ran out of memory after " + i + " strings were created.");
+        }
     }
 }
