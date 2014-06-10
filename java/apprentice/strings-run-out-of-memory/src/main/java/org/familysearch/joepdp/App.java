4089b2ea884c24c82e6887913f7bb9a9b2b4f168
Joe Shull <shullja@familysearch.org>
2014-06-10 15:50:03 -0600

    cleaned up the code a bit.  Added a byte count output in addiiton to the number of strings output.

--- java/apprentice/strings-run-out-of-memory/src/main/java/org/familysearch/joepdp/App.java
+++ java/apprentice/strings-run-out-of-memory/src/main/java/org/familysearch/joepdp/App.java
@@ -1,20 +1,23 @@
 package org.familysearch.joepdp;
 
+import java.text.NumberFormat;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
+
 
 
 public class App {
-    private static String padTo(String original, int length, char padCharacter){
-        String out = original;
-        for(int i=0; i<length-original.length(); i++) {
-            out = "" + padCharacter + out;
-        }
-        return out;
-    }
+    private static final int STRING_LENGTH = 100;
+//                                            0000000000111111111122222222223333333333444444444455555555556666666666777777777788888888889999999999
+//                                            0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+    private static final String PAD_STRING = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
     private static String generateEntry(int index){
-        String numString = "" + index;
-        return padTo(numString, 100, '0');
+        StringBuilder out = new StringBuilder(STRING_LENGTH);
+        out.append(index);
+        out.insert(0, PAD_STRING,0,PAD_STRING.length()- out.length());
+        return out.toString();
     }
 
     public static void main(String[] args){
@@ -26,18 +29,18 @@ public class App {
                 i++;
             }
         } catch (OutOfMemoryError e) {
-            listOfStrings = null; // give the garbage collector a chance to run if necessary
+            listOfStrings = null; // allow the garbage collector to reclaim the space
             System.err.println("Exception: " + e.getMessage());
             e.printStackTrace(System.err);
         } catch (Exception e) {
-            listOfStrings = null; // give the garbage collector a chance to run if necessary
+            listOfStrings = null; //  allow the garbage collector to reclaim the space
             System.err.println("Exception: " + e.getMessage());
             e.printStackTrace(System.err);
         }
-        if(listOfStrings!=null){
-            System.out.println("Quitting in a completely unanticiped way...");
-        } else {
-            System.out.println("Ran out of memory after " + i + " strings were created.");
-        }
+        // Minimum String memory usage (bytes) = 8 * (int) ((((no chars) * 2) + 45) / 8)
+        // from: http://www.javamex.com/tutorials/memory/string_memory_usage.shtml
+        System.out.println("Ran out of memory after " + i + " strings were created.");
+        int bytes = i * (8 * (((STRING_LENGTH * 2) + 45) / 8));
+        System.out.println("That corresponds to " + NumberFormat.getNumberInstance(Locale.US).format(bytes) + " bytes of memory.");
     }
 }
