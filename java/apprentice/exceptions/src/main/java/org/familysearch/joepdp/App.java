c0860d2050bcf6f1399b11950899dbae02424a06
Joe Shull <shullja@familysearch.org>
2014-06-12 10:21:24 -0600

    implemented code

--- java/apprentice/exceptions/src/main/java/org/familysearch/joepdp/App.java
+++ java/apprentice/exceptions/src/main/java/org/familysearch/joepdp/App.java
@@ -1,13 +1,26 @@
 package org.familysearch.joepdp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println("Going to throw a checked exception...");
+        try{
+            throw(new Exception("This is a checked exception"));
+        } catch (Exception e){
+            System.out.println("Caught " + e.getClass() + " with message: \"" + e.getMessage() + "\"");
+        }
+
+        System.out.println("Going to throw an unchecked exception...");
+        try{
+            throw(new RuntimeException("This is an unchecked exception"));
+        } catch (Exception e){
+            System.out.println("Caught " + e.getClass() + " with message: \"" + e.getMessage() + "\"");
+        }
+
+        System.out.println("Going to throw an error exception...");
+        try{
+            throw(new Error("This is an error exception"));
+        } catch (Throwable t){
+            System.out.println("Caught " + t.getClass() + " with message: \"" + t.getMessage() + "\"");
+        }
     }
 }
