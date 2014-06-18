628cedd5b8c425e36a550f17646fde7b85b149b2
Joe Shull <shullja@familysearch.org>
2014-06-18 14:10:34 -0600

    pretty much implemented

--- java/apprentice/instanceof/src/main/java/org/familysearch/joepdp/App.java
+++ java/apprentice/instanceof/src/main/java/org/familysearch/joepdp/App.java
@@ -1,13 +1,21 @@
 package org.familysearch.joepdp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        doSomethingWithThisObject(42);
+        doSomethingWithThisObject("Hello World");
+        doSomethingWithThisObject(Math.PI);
+    }
+
+    public static void doSomethingWithThisObject(Object o){
+        if(o instanceof Integer){
+            Integer integer = (Integer) o;
+            System.out.println("You sent me an Integer whose value is: [" + integer + "]");
+        } else if (o instanceof String){
+            String string = (String) o;
+            System.out.println("You sent me a String which contains: \"" + string + "\"");
+        } else {
+            System.out.println("You sent me a " + o.getClass().getSimpleName() + " whose string representation is {" + o.toString() + "}");
+        }
     }
 }
