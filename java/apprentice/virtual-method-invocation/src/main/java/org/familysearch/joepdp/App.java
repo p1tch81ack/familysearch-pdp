0fc07b7141cea3922adc271a838d016b4c0020b6
Joe Shull <shullja@familysearch.org>
2014-06-18 13:38:55 -0600

    pretty much implemented

--- java/apprentice/virtual-method-invocation/src/main/java/org/familysearch/joepdp/App.java
+++ java/apprentice/virtual-method-invocation/src/main/java/org/familysearch/joepdp/App.java
@@ -1,13 +1,28 @@
 package org.familysearch.joepdp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+interface StuffDoer{
+    void doStuff();
+}
+
+class Foo implements StuffDoer {
+    @Override
+    public void doStuff() {
+        System.out.println("Doing Foo Stuff!");
+    }
+}
+
+class Bar implements StuffDoer {
+    @Override
+    public void doStuff() {
+        System.out.println("Doing Bar Stuff!");
+    }
+}
+
+public class App {
+    public static void main( String[] args ) {
+        StuffDoer foo = new Foo();
+        StuffDoer bar = new Bar();
+        foo.doStuff();
+        bar.doStuff();
     }
 }
