0d459f34005cfa7b063bef4f1afcd6c96879f5f0
Joe Shull <shullja@familysearch.org>
2014-06-20 10:48:50 -0600

    initial project creation

---
+++ java/apprentice/overloading-methods/src/main/java/org/familysearch/joepdp/App.java
@@ -0,0 +1,13 @@
+package org.familysearch.joepdp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
