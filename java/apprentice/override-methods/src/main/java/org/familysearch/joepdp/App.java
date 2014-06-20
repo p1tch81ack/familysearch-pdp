92c1fa10698ec446920a0865095e1cba6930705f
Joe Shull <shullja@familysearch.org>
2014-06-20 10:23:03 -0600

    implemented

--- java/apprentice/override-methods/src/main/java/org/familysearch/joepdp/App.java
+++ java/apprentice/override-methods/src/main/java/org/familysearch/joepdp/App.java
@@ -1,13 +1,23 @@
 package org.familysearch.joepdp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class Foo {
+    public void doSomething(){
+        System.out.println("This is Foo doing something");
+    }
+}
+
+class Bar extends Foo {
+    @Override
+    public void doSomething() {
+        System.out.println("This is Bar doing something");
+    }
+}
+
+public class App {
+    public static void main( String[] args ) {
+        Foo foo = new Foo();
+        foo.doSomething();
+        Foo bar = new Bar();
+        bar.doSomething();
     }
 }
