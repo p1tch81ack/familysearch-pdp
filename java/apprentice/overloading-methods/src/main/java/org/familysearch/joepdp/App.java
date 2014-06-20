aabdaa52bc4a821a359a2a6fe2293f984e0f14bf
Joe Shull <shullja@familysearch.org>
2014-06-20 10:53:59 -0600

    implemented

--- java/apprentice/overloading-methods/src/main/java/org/familysearch/joepdp/App.java
+++ java/apprentice/overloading-methods/src/main/java/org/familysearch/joepdp/App.java
@@ -1,13 +1,22 @@
 package org.familysearch.joepdp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class Foo{
+    public void doSomething(int i){
+        System.out.println("You sent me an Integer whose value is: [" + i + "]");
+    }
+    public void doSomething(String s){
+        System.out.println("You sent me a String which contains: \"" + s + "\"");
+    }
+    public void doSomething(Object o){
+        System.out.println("You sent me a " + o.getClass().getSimpleName() + " whose string representation is {" + o.toString() + "}");
+    }
+}
+
+public class App {
+    public static void main( String[] args ) {
+        Foo foo = new Foo();
+        foo.doSomething(42);
+        foo.doSomething("Hello World");
+        foo.doSomething(Math.PI);
     }
 }
