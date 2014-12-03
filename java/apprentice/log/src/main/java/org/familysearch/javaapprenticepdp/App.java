bfc13ad391b59014fe7ff7b75d8b655b71f43ea3
Joe Shull <shullja@familysearch.org>
2014-12-03 14:45:52 -0700

    initial project creation

---
+++ java/apprentice/log/src/main/java/org/familysearch/javaapprenticepdp/App.java
@@ -0,0 +1,13 @@
+package org.familysearch.javaapprenticepdp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
