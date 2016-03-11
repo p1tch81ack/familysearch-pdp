534e355ca4ebd5773684e98390a1a5f86e1f1952
Joe Shull <shullja@familysearch.org>
2016-03-10 20:17:47 -0700

    copied from rest 1 (apprentice)

--- rest/journeyman/rest2/src/main/java/org/familysearch/joepdp/rest2/Folder.java
+++ rest/journeyman/rest2/src/main/java/org/familysearch/joepdp/rest2/Folder.java
@@ -1,9 +1,8 @@
-package org.familysearch.joepdp.rest;
+package org.familysearch.joepdp.rest2;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
